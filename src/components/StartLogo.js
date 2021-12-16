480b205858ab166185214bcdc2399f6b338e4eaf
mateuskuritza <mateuskuritza@gmail.com>
2021-12-16 10:54:55 -0300

    Preparando revisão de código

--- src/components/StartLogo.js
+++
@@ -1,8 +0,0 @@
-export default function StartLogo({setScreen}) {
-    return (
-    <section className="start-logo" onClick={() => setScreen(true)}>
-        <img src="./assets/Vector.svg"alt="imagem de um raio"/>
-        <span>ZapRecall</span>
-    </section>
-    )
-}
--- src/utils/returnClass.js
+++
@@ -1,7 +0,0 @@
-export default function returnClass(value) {
-    if(value) {
-        return "front"
-    } else {
-        return "back"
-    }
-}
